56bf6cf84770a08bce32154660ee94534c3f2541
amanddaluz <amandaomariano@hotmail.com>
2022-06-08 19:03:24 -0300

    setando a ação do botton navigation criação do layout favorites setando a marvelFont

---
+++ app/src/main/java/com/amandaluz/marvelproject/HomeActivity.kt
@@ -0,0 +1,24 @@
+package com.amandaluz.marvelproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.amandaluz.marvelproject.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNavigator.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/amandaluz/marvelproject/favorite/FavoriteFragment.kt
@@ -0,0 +1,20 @@
+package com.amandaluz.marvelproject.favorite
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.amandaluz.marvelproject.R
+
+class FavoriteFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorite, container, false)
+    }
+
+}
