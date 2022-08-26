7a53878309d2e970babd7c74cceebcb299090522
amanddaluz <amandaluz@brq.com>
2022-08-26 18:01:55 -0300

    inicio da implementação da SplashActivity

---
+++ app/src/main/java/com/amandaluz/marvelproject/view/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.amandaluz.marvelproject.view.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.amandaluz.marvelproject.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
